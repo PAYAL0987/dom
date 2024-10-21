a93c6d2a1be5bd340213e1e91c89023b2c0713ab
PAYAL0987 <payalkumari9910@gmail.com>
2024-10-21 13:10:08 +0530

    added file

---
+++ script.js
@@ -0,0 +1,24 @@
+function styleDiv() {
+    
+    var div = document.getElementById('targetDiv');
+
+    div.style.backgroundColor = 'yellow';
+
+    
+    div.style.margin = '20px';
+
+  
+    div.style.padding = '10px';
+
+    
+    div.style.fontSize = '18px';
+
+    
+    div.style.fontWeight = 'bold';
+
+    
+    div.style.height = '200px';
+
+    
+    div.style.width = '300px';
+}
